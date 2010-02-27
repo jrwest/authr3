04c31f855b27e631dc6710d933593a870913e4e0
jrwest <jordan.west@westwardwd.com>
2010-02-27 01:38:23 -0500

    Account model uses bcrypt to safely store passwords
    for now bcrypt must included in the including applications Gemfile.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,12 @@
 class Account < ActiveRecord::Base
+  include BCrypt
 
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
 end
