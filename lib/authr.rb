36af53a6a584faff320cc16f9b7357f16c372ff1
jrwest <jordan.west@westwardwd.com>
2010-02-27 15:29:55 -0500

    use ActionController::Base.authenticate to require autentication for your actions.
    the authenticate macro (class method) when called alone require a valid session for any route except the new session and create session. To modify this behaviour use :only and :except in the same manner you would for filters. For example, :except => [:index] will allow the index action of the controller to be invoked without a valid session. Be careful with this however, right now for example, if you add this to your application controller any index action will will be able to invoked. There will be better ways for doing this soon.
    
    Note: authenticate cannot be called on ActionController::Base it must be called on ApplicationController or its Subclasses.

--- lib/authr.rb
+++ lib/authr.rb
@@ -1,3 +1,5 @@
 module Authr
   require 'authr/engine' if defined?(Rails)
 end
+
+require 'extensions/action_controller/base'
---
+++ lib/extensions/action_controller/base.rb
@@ -0,0 +1,19 @@
+class ActionController::Base
+  def self.authenticate(options = {})
+    raise Exception, "Authenticate cannot be called on ActionController::Base. Only it's subclasses" if self ==  ActionController::Base
+    prepend_before_filter :authenticate_session, options
+  end
+
+  private
+
+    def authenticate_session
+      unless valid_session? || [new_session_path, session_path].include?(request.request_uri)
+        redirect_to new_session_path 
+      end
+    end
+
+    def valid_session?
+      ((session[:account_id] && session[:account_id] > 0) && (session[:account_login_time] && session[:account_login_time] < Time.now))
+    end
+
+end
